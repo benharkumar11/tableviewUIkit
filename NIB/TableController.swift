6e8aeaf6e0dae2ed187501acfada8fda4cc72034
benharkumar <benharkumar11@gmail.com>
2024-09-03 17:59:07 -0400

    NIB

---
+++ NIB/TableController.swift
@@ -0,0 +1,8 @@
+//
+//  TableController.swift
+//  NIB
+//
+//  Created by Benhar Kumar on 9/3/24.
+//
+
+import Foundation
---
+++ NIB/XIBTVC.swift
@@ -0,0 +1,23 @@
+//
+//  XIBTVC.swift
+//  NIB
+//
+//  Created by Benhar Kumar on 9/3/24.
+//
+
+import UIKit
+
+class XIBTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
